f6905058eba3f763a2d8555caa98da1918b69fbc
McTwist <rajb89@hotmail.com>
2015-06-15 23:03:54 +0200

    Made the class more useful.

--- src/classes/user.class.php
+++ src/classes/user.class.php
@@ -4,6 +4,17 @@ class User
 {
 	private $id = 0;
 	private $username = '';
+
+	public function __construct($id, $username)
+	{
+		$this->id = $id;
+		$this->username = $username;
+	}
+
+	public function Name()
+	{
+		return $this->username;
+	}
 }
 
 ?>
