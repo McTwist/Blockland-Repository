1e96b806608312ca11d8c7bedf79130bf9567263
McTwist <rajb89@hotmail.com>
2015-06-30 18:56:39 +0200

    Created Input class.

---
+++ src/classes/input.class.php
@@ -0,0 +1,170 @@
+<?php
+
+class Input
+{
+	private $get = [];
+	private $post = [];
+	private $files = [];
+	private $data = [];
+	private $raw = null;
+	private $raw_get = null;
+
+	// Loads all the data available as input
+	public function Process()
+	{
+		switch (strtoupper($_SERVER['REQUEST_METHOD']))
+		{
+		case 'POST':
+			$this->RequestPost();
+			// Fallthrough as you can still send get variables
+		case 'GET':
+			$this->RequestGet();
+			break;
+		}
+
+		$this->CreateData();
+	}
+
+	// Move files to a special folder
+	public function MoveFiles($folder)
+	{
+		if (substr($folder, -1) !== '/')
+			$folder .= '/';
+		$success = 0;
+		foreach ($this->files as $name => $files)
+		{
+			foreach ($files as $i => $file)
+			{
+				if ($file['error'] === 0 && 
+					move_uploaded_file($file['tmp_name'], $folder.basename($file['name'])))
+				{
+					$this->files[$name][$i]['new_name'] = $folder.basename($file['name']);
+					++$success;
+				}
+			}
+		}
+
+		// Recreate the data
+		if ($success > 0)
+			$this->CreateData();
+
+		return $success;
+	}
+
+	// Get the amount of files uploaded
+	public function FileCount()
+	{
+		$amount = 0;
+		foreach ($this->files as $files)
+			$amount += count($files);
+		return $amount;
+	}
+
+	// How many file errors that occured
+	public function FileErrors()
+	{
+		$amount = 0;
+		foreach ($this->files as $files)
+			foreach ($files as $file)
+				if ($file['error'] !== 0)
+					++$amount;
+		return $amount;
+	}
+
+	public function Get()
+	{
+		return $this->get;
+	}
+
+	public function Post()
+	{
+		return $this->post;
+	}
+
+	public function Files()
+	{
+		return $this->files;
+	}
+
+	public function Data()
+	{
+		return $this->data;
+	}
+
+	public function Raw()
+	{
+		return $this->raw;
+	}
+
+	public function RawGet()
+	{
+		return $this->raw_get;
+	}
+
+	// Request the get variables
+	private function RequestGet()
+	{
+		$this->raw_get = $_SERVER['QUERY_STRING'];
+		if (empty($this->raw_get))
+			return;
+
+		parse_str($this->raw_get, $this->get);
+	}
+
+	// Request the post variables
+	private function RequestPost()
+	{
+		// May contain a boundary, so skipping that part
+		list($type) = explode(';', $_SERVER['CONTENT_TYPE'], 2);
+
+		switch (strtolower($type))
+		{
+		// Normal way to do it
+		case 'application/x-www-form-urlencoded':
+			$this->raw = file_get_contents('php://input');
+			parse_str($this->raw, $this->post);
+			break;
+		// Sent files
+		case 'multipart/form-data':
+			$this->post = $_POST;
+
+			foreach ($_FILES as $name => $data)
+			{
+				$this->files[$name] = [];
+				// Multiple files
+				if (is_array($data['name']))
+				{
+					// Convert to a more convenient format
+					foreach ($data as $key => $dat)
+						foreach ($dat as $i => $val)
+							$this->files[$name][$i][$key] = $val;
+				}
+				// Single file
+				else
+				{
+					array_push($this->files[$name], $data);
+				}
+			}
+			break;
+		// Special JSON case
+		case 'application/json':
+			$this->raw = file_get_contents('php://input');
+			$this->post = json_decode($this->raw);
+			break;
+		// Handle unknown data
+		default:
+			// Just store it directly
+			$this->raw = file_get_contents('php://input');
+			break;
+		}
+	}
+
+	private function CreateData()
+	{
+		// Merge get, post and files
+		// Note: They will overwrite each other. Adding to a list could be an option.
+		$this->data = array_merge($this->get, $this->post, $this->files);
+	}
+}
+
+?>
