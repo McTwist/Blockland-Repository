56a86364cbed97d8b5d644274c44db30a95b13a0
McTwist <rajb89@hotmail.com>
2015-08-05 21:18:36 +0200

    Add support for RTB
    Includes rtbInfo.txt.
    This is for compatibility only.

--- src/classes/file.class.php
+++ src/classes/file.class.php
@@ -23,6 +23,9 @@ class File
 	// version.txt
 	private $version = null;
 
+	// rtbInfo.txt
+	private $rtbInfo = null;
+
 	const NL = "\r\n";
 
 	public function __construct($file)
@@ -49,11 +52,13 @@ class File
 
 		$this->description = new FileDescription();
 		$this->version = new FileVersion();
+		$this->rtbInfo = new FileRTBInfo();
 
 		// Read in default information if it exists
 		$this->ReadVersion();
 		$this->ReadDescription();
 		$this->ReadNamecheck();
+		$this->ReadRTB();
 
 		// Count all file types
 		for ($i = 0; $i < $this->archive->numFiles; $i++)
@@ -408,6 +413,45 @@ class File
 		if (!empty($content))
 			$this->archive->addFromString($json ? 'version.json' : 'version.txt', $content);
 	}
+
+	// RTB info
+	// Validate rtbInfo.txt
+	public function ValidateRTB()
+	{
+		return $this->rtbInfo->Validate();
+	}
+
+	// Read rtbInfo.txt
+	public function ReadRTB()
+	{
+		if ($this->HaveFile('rtbInfo.txt'))
+			$this->rtbInfo->Read($this->ReadFile('rtbInfo.txt'));
+	}
+
+	// Generate a rtbInfo file
+	public function GenerateRTB($overwrite = false)
+	{
+		if (!$overwrite && $this->HaveFile('rtbInfo.txt'))
+			return;
+
+		$content = $this->rtbInfo->Generate();
+
+		// Save it
+		if (!empty($content))
+			$this->archive->addFromString('rtbInfo.txt', $content);
+	}
+
+	// Check if RTB exists
+	public function HaveRTB()
+	{
+		return $this->HaveFile('rtbInfo.txt');
+	}
+
+	// Remove RTB
+	public function RemoveRTB()
+	{
+		return $this->RemoveFile('rtbInfo.txt');
+	}
 }
 
 /*
@@ -733,6 +777,127 @@ class FileVersion
 	public function Repositories()
 	{
 		return $this->repositories;
+	}
+}
+
+/*
+ * FileRTBInfo
+ * Handles the rtbinfo file
+ * Return To Blockland
+ */
+class FileRTBInfo
+{
+	private $id = '';
+	private $icon = '';
+	private $type = '';
+	private $title = '';
+	private $version = '';
+
+	// It's constant, as that is how RTB handled it
+	const NL = "\r\n";
+
+	// Read the info
+	public function Read($content)
+	{
+		// Split it into suitable pieces
+		$lines = preg_split('/$\R?^/m', $content);
+
+		foreach ($lines as $line)
+		{
+			// Id
+			if (substr($line, 0, 3) == 'id:')
+			{
+				$this->id = trim(substr($line, 3));
+			}
+			// Icon
+			elseif (substr($line, 0, 5) == 'icon:')
+			{
+				$this->icon = trim(substr($line, 5));
+			}
+			// Type
+			elseif (substr($line, 0, 5) == 'type:')
+			{
+				$this->type = trim(substr($line, 5));
+			}
+			// Title
+			elseif (substr($line, 0, 6) == 'title:')
+			{
+				$this->title = trim(substr($line, 6));
+			}
+			// Version
+			elseif (substr($line, 0, 8) == 'version:')
+			{
+				$this->version = trim(substr($line, 8));
+			}
+		}
+	}
+
+	// Validate the info
+	public function Validate()
+	{
+		return !empty($this->id)
+			&& !empty($this->icon)
+			&& !empty($this->type)
+			&& !empty($this->title)
+			&& !empty($this->version);
+	}
+
+	// Generate new description content
+	public function Generate()
+	{
+		// I can't let you do that
+		if (!$this->Validate())
+			return null;
+
+		// Prepare the data
+		$content = '';
+		$content .= "id: {$this->id}".self::NL;
+		$content .= "icon: {$this->icon}".self::NL;
+		$content .= "type: {$this->type}".self::NL;
+		$content .= "title: {$this->title}".self::NL;
+		$content .= "version: {$this->version}".self::NL;
+
+		return $content;
+	}
+
+	public function Id($value = null)
+	{
+		$id = $this->id;
+		if ($value !== null)
+			$this->id = $value;
+		return $id;
+	}
+
+	public function Icon($value = null)
+	{
+		$icon = $this->icon;
+		if ($value !== null)
+			$this->icon = $value;
+		return $icon;
+	}
+
+	public function Type($value = null)
+	{
+		$type = $this->type;
+		if ($value !== null)
+			$this->type = $value;
+		return $type;
+	}
+
+	public function Title($value = null)
+	{
+		$title = $this->title;
+		if ($value !== null)
+			$this->title = $value;
+		return $title;
+	}
+
+	public function Version($value = null)
+	{
+		$version = $this->version;
+		if ($value !== null)
+			$this->version = $value;
+		return $version;
 	}
 }
 
