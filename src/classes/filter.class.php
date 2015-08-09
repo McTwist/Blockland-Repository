f3cea650a3baac890e0b1bd5d51da9da181c7091
McTwist <rajb89@hotmail.com>
2015-08-09 22:49:06 +0200

    Add first version of a filter system

---
+++ src/classes/filter.class.php
@@ -0,0 +1,47 @@
+<?php
+
+class Filter
+{
+	// Filter through the username and return a valid one
+	static public function Username($value)
+	{
+		return filter_var($value, FILTER_SANITIZE_STRING);
+	}
+
+	// Validate a BLID
+	static public function BLID($value)
+	{
+		$opt = array(
+			'options' => array(
+				'default' => null,
+				'min_range' => 0,
+				'max_range' => 999999
+			)
+		);
+		return filter_var($value, FILTER_VALIDATE_INT, $opt);
+	}
+
+	// Validate an e-mail
+	static public function Email($value)
+	{
+		return filter_var($value, FILTER_VALIDATE_EMAIL, self::DefaultOptions());
+	}
+
+	// Validate an IP-address
+	static public function IP($value)
+	{
+		return filter_var($value, FILTER_VALIDATE_IP, self::DefaultOptions());
+	}
+
+	// Create a default array of options
+	static private function DefaultOptions($default = null)
+	{
+		return array(
+			'options' => array(
+				'default' => $default
+			)
+		);
+	}
+}
+
+?>
