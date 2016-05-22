5b24a7c4eaeea99df26ca09f2545413d5feb572f
mctwist <rajb89@hotmail.com>
2016-05-22 14:34:19 +0200

    Made Auth module work with our User model

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
+use App\Models\User;
 use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
@@ -49,8 +49,8 @@ class AuthController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
+            'username' => 'required|max:32|unique:users',
+            'email' => 'required|email|max:254|unique:users',
             'password' => 'required|min:6|confirmed',
         ]);
     }
@@ -64,7 +64,7 @@ class AuthController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'username' => $data['username'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Authenticatable as AuthenticatableTrait;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 
-class User extends Model
+class User extends Model implements AuthenticatableContract
 {
+	use AuthenticatableTrait;
+
 	protected $hidden = ['id', 'password'];
+
+	protected $fillable = ['username', 'email', 'password'];
 
 	public function addons()
 	{
