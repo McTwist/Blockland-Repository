b5f52ae5332935bbeaa07f9f0878e12b9814fe4b
mctwist <rajb89@hotmail.com>
2016-06-01 07:45:41 +0200

    Made category.show to work properly

--- resources/views/resources/categories/show.blade.php
+++ resources/views/resources/categories/show.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.master')
 
+@section('mainbox', 'main')
+
 @section('content')
 
-	<h2>{{ $category->name }} Category</h2>
+	<h2>{{ $category->name }}</h2>
+	<div id="addons">
+
+		@forelse($category->addons()->get() as $addon)
+			<a href="{{ route('addon.show', $addon->slug) }}"><div>{{ $addon->name }}</div></a>
+		@empty
+			Unknown category
+		@endforelse
+
+	</div>
 
 @endsection
