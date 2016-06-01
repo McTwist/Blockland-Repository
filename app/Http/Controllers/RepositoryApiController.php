79be06d5c8cd654e30445809100d310608909cff
mctwist <rajb89@hotmail.com>
2016-06-01 23:35:13 +0200

    Implemented database into Repository API
    * That includes attributes, like pretty
    * Supports both Laravel and GET style

--- app/Http/Controllers/RepositoryApiController.php
+++ app/Http/Controllers/RepositoryApiController.php
@@ -6,54 +6,96 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\Models\Addon;
+
 class RepositoryApiController extends Controller
 {
-	// TODO: Implement each and all of the methods (Need database for that)
-	// Default page with all add-ons for default repository
+	// Default page with GET method
 	public function home(Request $request)
 	{
+		$pretty = isset($request->pretty);
 		if ($request->has('mods'))
 		{
-			return $this->mods($request->get('mods'));
+			return $this->mods($request->get('mods'), $pretty);
 		}
 		elseif ($request->has('mod'))
 		{
-			return $this->mod($request->get('mod'));
+			return $this->mod($request->get('mod'), $pretty);
 		}
 		elseif ($request->has('repo'))
 		{
-			return $this->repository($request->get('repo'));
+			return $this->repository($request->get('repo'), $pretty);
 		}
 		else
 		{
-			return self::json(
-				(object)[
-					"addons"=>
-					[
-						(object)[
-							"name"=>"Stu//ff",
-						]
-					]
-				], isset($request->pretty));
+			return $this->repository('', $pretty);
 		}
 	}
 
 	// Display all mods in a list
-	public function mods($mods)
+	public function mods($data, $pretty = null)
 	{
-		return self::json((object)[$mods]);
+		$mods = self::ExtractData($data, $data);
+		$addon_slugs = explode('-', $mods);
+		$addons = Addon::whereIn('slug', $addon_slugs)->get();
+		$obj = self::ObjectFromArray($addons);
+		return self::json($obj, isset($pretty) || self::IsPretty($data));
 	}
 
 	// Display only one mod
-	public function mod($mod)
+	public function mod($data, $pretty = null)
 	{
-		return self::json((object)[$mod]);
+		$mod = self::ExtractData($data, $data);
+		$addon = Addon::where('slug', $mod)->first();
+		$obj = self::ObjectFromArray([$addon]);
+		return self::json($obj, isset($pretty) || self::IsPretty($data));
 	}
 
 	// Display a certain repository and all its add-ons
-	public function repository($repo)
+	public function repository($data, $pretty = null)
 	{
-		return self::json((object)[$repo]);
+		$repo = self::ExtractData($data, $data);
+		$addons = Addon::all();
+		$obj = self::ObjectFromArray($addons);
+		return self::json($obj, isset($pretty) || self::IsPretty($data));
+	}
+
+	// Extract data and attributes from string
+	private static function ExtractData($data, &$attributes)
+	{
+		$attributes = explode('&', $data);
+		return array_shift($attributes);
+	}
+
+	// Check if data is pretty
+	private static function IsPretty(array $data)
+	{
+		return count($data) > 0 && in_array('pretty', $data);
+	}
+
+	// Get object out from array of addons
+	static private function ObjectFromArray($addons)
+	{
+		$obj = new \stdClass;
+		$obj->addons = [];
+		foreach ($addons as $addon)
+		{
+			$obj->addons[] = self::ObjectFromAddon($addon);
+		}
+		return $obj;
+	}
+
+	// Get object out from addon
+	static private function ObjectFromAddon(Addon $addon)
+	{
+		$obj = new \stdClass;
+		$obj->name = $addon->name;
+		$obj->id = $addon->slug;
+		$obj->description = $addon->description;
+		$obj->channels = [];
+		// TODO: Next sprint, add channels
+
+		return $obj;
 	}
 
 	static private function json($data, $pretty = false)
