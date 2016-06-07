7ee3348420d5854ccb986891be1fa95292a5ad71
mctwist <rajb89@hotmail.com>
2016-06-07 12:55:47 +0200

    Added listSelect to Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -14,4 +14,16 @@ class Category extends Model
 	{
 		return $this->hasMany(Addon::class);
 	}
+
+	// Get list of categories used for selects
+	public static function listSelect()
+	{
+		$cats = self::select('id', 'name')->get();
+		$categories = [];
+		foreach ($cats as $category)
+		{
+			$categories[(int)$category->id] = $category->name;
+		}
+		return $categories;
+	}
 }
