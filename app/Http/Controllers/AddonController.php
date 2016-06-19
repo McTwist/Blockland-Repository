828856288a8678a70c2a71fe643ecb4d55945e06
mctwist <rajb89@hotmail.com>
2016-06-19 23:02:51 +0200

    Applied size control on addon

--- app/Http/Controllers/AddonController.php
+++ app/Http/Controllers/AddonController.php
@@ -42,7 +42,7 @@ class AddonController extends Controller
 	public function upload(Request $request)
 	{
 		$this->validate($request, [
-			'addon' => 'required|mimes:zip'
+			'addon' => 'required|mimes:zip|min:512|max:52428800'
 		]);
 
 		if (!$request->session()->has('upload'))
