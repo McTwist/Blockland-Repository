3d1bb653b923965e99dbab396658ceaeec8381bc
mctwist <rajb89@hotmail.com>
2016-06-29 07:05:55 +0200

    Fixed migration error

--- database/migrations/2016_06_25_000530_create_channels_table.php
+++ database/migrations/2016_06_25_000530_create_channels_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateChannels extends Migration
+class CreateChannelsTable extends Migration
 {
     /**
      * Run the migrations.
