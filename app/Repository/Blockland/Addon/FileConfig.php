90a306175fdc00aa2082f94252d2185f3c9e4f17
mctwist <rajb89@hotmail.com>
2016-09-21 11:57:47 +0200

    Added FileConfig

---
+++ app/Repository/Blockland/Addon/FileConfig.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Repository\Blockland\Addon;
+
+use App\Repository\Archive\ArchiveFile;
+
+/*
+ * FileConfig
+ * Handles the config files
+ */
+class FileConfig extends ArchiveFile
+{
+	private $type = null;
+	private $variables = array();
+	private $lists = array();
+	private $content = null;
+
+	// Errors
+	private $errorInvalidVariable = false;
+
+	const NL = File::NL;
+
+	// Read the config information
+	public function Set($content)
+	{
+		$this->content = $content;
+		// Split it into suitable pieces
+		$lines = preg_split('/$\R?^/m', $content);
+
+		foreach ($lines as $line)
+		{
+			$line = ltrim($line);
+			// Empty line
+			if (empty($line))
+				continue;
+			// Comment
+			if (substr($line, 0, 2) == '//')
+				continue;
+
+			list($var, $value) = preg_split('/\s+/', $line, 2);
+
+			// Normal variable
+			if ($var[0] == '$')
+			{
+				$vars = $this->ParseVariable($var);
+				if ($vars !== false)
+				{
+					// Set chain of array branches
+					$node = &$this->variables;
+					foreach ($vars as $key)
+					{
+						// Make sure array exists
+						if (!array_key_exists($key, $node))
+							$node[$key] = array();
+						// Move aside the value
+						elseif (!is_array($node[$key]))
+							$node[$key] = array($node[$key]);
+						// Iterate node
+						$node = &$node[$key];
+					}
+					// Save variable
+					if (is_array($node) && count($node) != 0)
+						array_unshift($node, $value);
+					else
+						$node = $value;
+				}
+				// Variable was not parsed properly, so mark this as a failure
+				else
+				{
+					$this->errorInvalidVariable = true;
+				}
+			}
+			// Lists
+			else
+			{
+				if (!array_key_exists($var, $this->lists))
+					$this->lists[$var] = array();
+				$this->lists[$var][] = $value;
+			}
+		}
+	}
+
+	// Validate the config
+	public function Validate()
+	{
+		return !$this->errorInvalidVariable;
+	}
+
+	// Generate new config content
+	public function Get()
+	{
+		// Content should not be modified
+		return null;
+	}
+
+	// Get variables
+	public function GetVariables()
+	{
+		return $this->variables;
+	}
+
+	// Get one variable
+	public function GetVariable($key)
+	{
+		$var = $this->ParseVariable($key);
+		if ($var === false)
+			return null;
+		// Set chain of array branches
+		$node = &$this->variables;
+		foreach ($var as $key)
+		{
+			// Make sure that it even exists
+			if (!array_key_exists($key, $node))
+				return null;
+			$node = &$node[$key];
+		}
+		// Handle node variable
+		return (is_array($node) && array_key_exists(0, $node)) ? $node[0] : $node;
+	}
+
+	// Get lists
+	public function GetLists()
+	{
+		return $this->lists;
+	}
+
+	// Get one list
+	public function GetList($key)
+	{
+		return array_key_exists($key, $this->lists) ? $this->lists[$key] : null;
+	}
+
+	// Get variable information
+	private function ParseVariable($var)
+	{
+		$ret = preg_match_all('/([A-Za-z0-9_]+)/', $var, $matches);
+		if ($ret !== false)
+			return $matches[0];
+		return false;
+	}
+}
+
+?>
