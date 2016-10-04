0e4895e9c1f6eb0bccbbc4a3fc685e3f79ede0f8
mctwist <rajb89@hotmail.com>
2016-10-04 20:00:03 +0200

    Added path helper

---
+++ app/Helpers/path.php
@@ -0,0 +1,6 @@
+<?php
+
+function path()
+{
+	return implode(DIRECTORY_SEPARATOR, func_get_args());
+}
--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\HelperServiceProvider::class,
 
     ],
 
