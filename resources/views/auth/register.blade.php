c05348dc5c4ca5fdd87da27db0095990e85de7d2
Demian Wright <demian.git@gmail.com>
2016-11-25 21:20:17 +0200

    Rewrote login & register forms & CSS to match the original concept

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,26 +1,48 @@
 @extends('layouts.master')
 
 @section('content')
-	
-	{{ Form::open(array('route' => 'user.register', 'method' => 'post')) }}
-		<div>
-			{{ Form::label('username', 'Name:') }}
-			{{ Form::text('username', old('username')) }}
+	{{ Form::open(array('route' => 'user.register', 'method' => 'post', 'class' => 'form-horizontal')) }}
+
+	<fieldset class="blr-fieldset">
+		<legend>Register</legend>
+		<hr>
+		<!-- Username -->
+		<div class="form-group">
+			{{ Form::label('username', 'Username:', ['class' => 'control-label control-label-blr nopad-r col-xs-4']) }}
+			<div class="col-xs-4">
+				{{ Form::text('username', old('username'), ['required' => 'true', 'class' => 'form-control blr-form-control']) }}
+			</div>
 		</div>
-		<div>
-			{{ Form::label('email', 'Email:') }}
-			{{ Form::email('email', old('email')) }}
+
+		<!-- Email -->
+		<div class="form-group">
+			{{ Form::label('email', 'Email:', ['class' => 'control-label control-label-blr nopad-r col-xs-4']) }}
+			<div class="col-xs-4">
+				{{ Form::email('email', old('email'), ['required' => 'true', 'class' => 'form-control blr-form-control']) }}
+			</div>
 		</div>
-		<div>
-			{{ Form::label('password', 'Password:') }}
-			{{ Form::password('password') }}
+
+		<!-- Password -->
+		<div class="form-group">
+			{{ Form::label('password', 'Password:', ['class' => 'control-label control-label-blr nopad-r col-xs-4']) }}
+			<div class="col-xs-4">
+				{{ Form::password('password', ['required' => 'true', 'class' => 'form-control blr-form-control']) }}
+			</div>
 		</div>
-		<div>
-			{{ Form::label('password_confirmation', 'Confirm Password:') }}
-			{{ Form::password('password_confirmation') }}
+
+		<!-- Password confirmation -->
+		<div class="form-group">
+			{{ Form::label('password_confirmation', 'Confirm Password:', ['class' => 'control-label control-label-blr nopad-r col-xs-4']) }}
+			<div class="col-xs-4">
+				{{ Form::password('password_confirmation', ['required' => 'true', 'class' => 'form-control blr-form-control']) }}
+			</div>
 		</div>
-		<div>
-			{{ Form::submit('Register') }}
+
+		<!-- Register button -->
+		<div class="form-group">
+			<div class="col-xs-4 col-xs-offset-4">
+				{{ Form::submit('Register', ['class' => 'btn  blr-btn btn-blr-submit pull-right']) }}
+			</div>
 		</div>
 		@if (count($errors) > 0)
 			<ul>
@@ -29,6 +51,7 @@
 				@endforeach
 			</ul>
 		@endif
+	</fieldset>
 	{{ Form::close() }}
 
 @endsection
