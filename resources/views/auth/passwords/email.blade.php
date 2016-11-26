0473505a78edc9e56fa9b864f2b5ff13c6474aea
Demian Wright <demian.git@gmail.com>
2016-11-26 23:40:41 +0200

    Made registration page responsive

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -25,7 +25,7 @@
 			</div>
 			@if (count($errors) > 0)
 				<div class="row">
-					<div class="col-sm-12">
+					<div class="col-xs-12">
 						<ul>
 							@foreach ($errors->all() as $error)
 								<li>{{ $error }}</li>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,51 +6,65 @@
 	<fieldset class="blr-fieldset">
 		<legend>Register</legend>
 		<hr>
-		<!-- Username -->
-		<div class="form-group">
-			{{ Form::label('username', 'Username:', ['class' => 'control-label control-label-blr nopad-r col-xs-4']) }}
-			<div class="col-xs-4">
-				{{ Form::text('username', old('username'), ['required' => 'true', 'class' => 'form-control blr-form-control']) }}
-			</div>
-		</div>
+		<div class="container-fluid">
+			<div class="row">
+				<!-- Username -->
+				<div class="row form-group">
+					<div class="col-sm-12 text-sm-left col-md-2 col-md-offset-2 hug-md-right">
+						{{ Form::label('username', 'Username:', ['class' => 'control-label control-label-blr']) }}
+					</div>
+					<div class="col-sm-12 col-md-4">
+						{{ Form::text('username', old('username'), ['required' => 'true', 'class' => 'form-control blr-form-control']) }}
+					</div>
+				</div>
 
-		<!-- Email -->
-		<div class="form-group">
-			{{ Form::label('email', 'Email:', ['class' => 'control-label control-label-blr nopad-r col-xs-4']) }}
-			<div class="col-xs-4">
-				{{ Form::email('email', old('email'), ['required' => 'true', 'class' => 'form-control blr-form-control']) }}
-			</div>
-		</div>
+				<!-- Email -->
+				<div class="row form-group">
+					<div class="col-sm-12 text-sm-left col-md-2 col-md-offset-2 hug-md-right">
+						{{ Form::label('email', 'Email:', ['class' => 'control-label control-label-blr']) }}
+					</div>
+					<div class="col-sm-12 col-md-4">
+						{{ Form::email('email', old('email'), ['required' => 'true', 'class' => 'form-control blr-form-control']) }}
+					</div>
+				</div>
 
-		<!-- Password -->
-		<div class="form-group">
-			{{ Form::label('password', 'Password:', ['class' => 'control-label control-label-blr nopad-r col-xs-4']) }}
-			<div class="col-xs-4">
-				{{ Form::password('password', ['required' => 'true', 'class' => 'form-control blr-form-control']) }}
-			</div>
-		</div>
+				<!-- Password -->
+				<div class="row form-group">
+					<div class="col-sm-12 text-sm-left col-md-2 col-md-offset-2 hug-md-right">
+						{{ Form::label('password', 'Password:', ['class' => 'control-label control-label-blr']) }}
+					</div>
+					<div class="col-sm-12 col-md-4">
+						{{ Form::password('password', ['required' => 'true', 'class' => 'form-control blr-form-control']) }}
+					</div>
+				</div>
 
-		<!-- Password confirmation -->
-		<div class="form-group">
-			{{ Form::label('password_confirmation', 'Confirm Password:', ['class' => 'control-label control-label-blr nopad-r col-xs-4']) }}
-			<div class="col-xs-4">
-				{{ Form::password('password_confirmation', ['required' => 'true', 'class' => 'form-control blr-form-control']) }}
-			</div>
-		</div>
+				<!-- Password confirmation -->
+				<div class="row form-group">
+					<div class="col-sm-12 text-sm-left col-md-2 col-md-offset-2 hug-md-right">
+						{{ Form::label('password_confirmation', 'Confirm Password:', ['class' => 'control-label control-label-blr']) }}
+					</div>
+					<div class="col-sm-12 col-md-4">
+						{{ Form::password('password_confirmation', ['required' => 'true', 'class' => 'form-control blr-form-control']) }}
+					</div>
+				</div>
 
-		<!-- Register button -->
-		<div class="form-group">
-			<div class="col-xs-4 col-xs-offset-4">
-				{{ Form::submit('Register', ['class' => 'btn blr-btn btn-blr-default pull-right']) }}
+				<!-- Register button -->
+				<div class="row form-group">
+					<div class="col-sm-12 col-md-2 col-md-offset-6">
+						{{ Form::submit('Register', ['class' => 'btn blr-btn btn-blr-default float-md-right']) }}
+					</div>
+				</div>
+				@if (count($errors) > 0)
+					<div class="col-xs-12">
+						<ul>
+							@foreach ($errors->all() as $error)
+								<li>{{ $error }}</li>
+							@endforeach
+						</ul>
+					</div>
+				@endif
 			</div>
 		</div>
-		@if (count($errors) > 0)
-			<ul>
-				@foreach ($errors->all() as $error)
-					<li>{{ $error }}</li>
-				@endforeach
-			</ul>
-		@endif
 	</fieldset>
 	{{ Form::close() }}
 
