9bc8a87a8701404e612078a7d1aeb4c60dba19c1
Demian Wright <demian.git@gmail.com>
2016-11-26 23:19:46 +0200

    New password reset view with responsive design

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -2,21 +2,40 @@
 
 @section('content')
 
-	{{ Form::open(['route' => 'password.email', 'method' => 'post']) }}
-		<div>
-			{{ Form::label('email', 'Email:') }}
-			{{ Form::email('email') }}
+	{{ Form::open(['route' => 'password.email', 'method' => 'post', 'class' => 'form-horizontal']) }}
+	<fieldset class="blr-fieldset">
+		<legend>Reset Password</legend>
+		<hr>
+		<div class="container-fluid">
+			<div class="row">
+				<!-- Email -->
+				<div class="row form-group">
+					<div class="col-sm-12 text-sm-left col-md-2 col-md-offset-2 hug-md-right">
+						{{ Form::label('email', 'Account Email:', ['class' => 'control-label control-label-blr']) }}
+					</div>
+					<div class="col-sm-12 col-md-4">
+						{{ Form::email('email','', ['required' => 'true', 'class' => 'form-control blr-form-control']) }}
+					</div>
+				</div>
+
+				<!-- Reset button -->
+				<div class="col-sm-12 form-group">
+					{{ Form::submit('Send Password Reset Link', ['class' => 'btn blr-btn btn-blr-default']) }}
+				</div>
+			</div>
+			@if (count($errors) > 0)
+				<div class="row">
+					<div class="col-sm-12">
+						<ul>
+							@foreach ($errors->all() as $error)
+								<li>{{ $error }}</li>
+							@endforeach
+						</ul>
+					</div>
+				</div>
+			@endif
 		</div>
-		<div>
-			{{ Form::submit('Send Password Reset Link') }}
-		</div>
-		@if (count($errors) > 0)
-			<ul>
-				@foreach ($errors->all() as $error)
-					<li>{{ $error }}</li>
-				@endforeach
-			</ul>
-		@endif
+	</fieldset>
 	{{ Form::close() }}
 
 @endsection
