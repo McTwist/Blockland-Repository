f5bd02cc9af06411943f0ca9e8965cc827da4dcc
mctwist <rajb89@hotmail.com>
2016-11-30 12:06:15 +0100

    Added DebugBar to facades

--- config/app.php
+++ config/app.php
@@ -230,6 +230,8 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
         'AddonValidator' => App\Validation\Addon\Facade::class,
 
