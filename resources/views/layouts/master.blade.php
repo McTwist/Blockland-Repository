6bc9abd4025a5953b0e2f21a1b899f23f80d59ab
Demian Wright <demian.git@gmail.com>
2016-11-30 20:10:44 +0200

    Improved front page styles, support xs size, fixed broken title, closes #15

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,54 +1,62 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<!-- Favicons -->
-		<link rel="icon" href="/favicon.ico">
-		<link rel="icon" href="/favicon_192x192.png" sizes="192x192">
-		<link rel="icon" href="/favicon_32x32.png" sizes="32x32">
-		<link rel="icon" href="/favicon_16x16.png" sizes="16x16">
+<head>
+	<!-- Favicons -->
+	<link rel="icon" href="/favicon.ico">
+	<link rel="icon" href="/favicon_192x192.png" sizes="192x192">
+	<link rel="icon" href="/favicon_32x32.png" sizes="32x32">
+	<link rel="icon" href="/favicon_16x16.png" sizes="16x16">
 
-		<!-- Title -->
-		<title>Blockland Repository - @yield('title')</title>
+	<!-- Title -->
+	<title>
+		@hasSection('title')
+			@yield('title') - Blockland Repository
+		@else
+			Blockland Repository
+		@endif
+	</title>
 
-		<!-- Metas -->
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<!-- Fonts -->
-		<link href='//fonts.googleapis.com/css?family=Ubuntu:400,700' rel='stylesheet' type='text/css'>
-		<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
-		<link href='//fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
+	<!-- Fonts -->
+	<link href='//fonts.googleapis.com/css?family=Ubuntu:400,700' rel='stylesheet' type='text/css'>
+	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+	<link href='//fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
 
-		<!-- Stylesheets -->
-		<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="/css/main.css">
-		<link rel="stylesheet" type="text/css" href="/css/form.css">
-		<link rel="stylesheet" type="text/css" href="/css/app.css">
+	<!-- Stylesheets -->
+	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/css/main.css">
+	<link rel="stylesheet" type="text/css" href="/css/form.css">
+	<link rel="stylesheet" type="text/css" href="/css/app.css">
 
-		@yield('stylesheets')
-	</head>
+	@yield('stylesheets')
+</head>
 
-	<body>
-		<div id="popup-box-wrapper" class="container container-table" style="display: none;">
-			<div id="popup-box-container" class="row vertical-center-row">
-			</div>
-		</div>
-		<div class="--center">
-			@include('partials.header')
+<body>
+<div id="popup-box-wrapper" class="container container-table" style="display: none;">
+	<div id="popup-box-container" class="row vertical-center-row">
+	</div>
+</div>
+<div class="--center">
+	@include('partials.header')
 
-			<div id="@yield('mainbox', 'main')" class="container low-box" role="document">
-				@include('partials.navigation')
+	<div id="@yield('mainbox', 'main')" class="container low-box" role="document">
+		@include('partials.navigation')
 
-				@yield('content', view('resources.categories.index'))
+		@yield('content', view('resources.categories.index'))
 
-				@yield('footer', view('partials.footer'))
-			</div>
-		</div>
+		@yield('footer', view('partials.footer'))
+	</div>
+</div>
 
-		@yield('scripts.footer')
+@yield('scripts.footer')
 
-		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-		<script type="text/javascript">if (!window.jQuery) { document.write('<script src="/js/jquery.min.js"><\/script>'); }</script>
-		<script type="text/javascript" src="/js/popup.js"></script>
-		@yield('scripts')
-	</body>
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script type="text/javascript">if (!window.jQuery) {
+		document.write('<script src="/js/jquery.min.js"><\/script>');
+	}</script>
+<script type="text/javascript" src="/js/popup.js"></script>
+@yield('scripts')
+</body>
 </html>
--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,37 +1,37 @@
-<div class="row">
-	<nav id="navigation" class="navigation col-xs-12" role="navigation">
-		<div class="row">
-			@if(Auth::check())
-				<div class="col-xs-12 col-sm-9 col-sm-push-3 col-md-10 col-md-push-2">
-					<div class="row">
-						<div class="col-xs-12">
-							<div class="text-xs-center text-sm-right">Welcome, {{ Auth::user()->username }}.
+{{-- Currently there is nothing to show on the navigation bar on the login page so skip the bar entirely. --}}
+@if(!Request::is('user/login'))
+	<div class="row">
+		<nav id="navigation" class="navigation col-xs-12" role="navigation">
+			<div class="row">
+				@if(Auth::check())
+					<div class="col-xs-12 col-sm-9 col-sm-push-3 col-md-10 col-md-push-2">
+						<div class="row">
+							<div class="col-xs-12">
+								<div class="text-xs-center text-sm-right">Welcome, {{ Auth::user()->username }}.
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-xs-12 text-xs-center text-sm-right">
+								<ul class="nav-item-group">
+									<li class="nav-item"><a href="{{ route('user.show') }}">My Profile</a></li>
+									<li class="nav-item"><a href="{{ route('user.logout') }}">Log Out</a></li>
+								</ul>
 							</div>
 						</div>
 					</div>
-					<div class="row">
-						<div class="col-xs-12 text-xs-center text-sm-right">
-							<ul class="nav-item-group">
-								<li class="nav-item"><a href="{{ route('user.show') }}">My Profile</a></li>
-								<li class="nav-item"><a href="{{ route('user.logout') }}">Log Out</a></li>
-							</ul>
-						</div>
+					<div class="col-xs-12 col-sm-3 col-sm-pull-9 col-md-2 col-md-pull-10">
+						<input id="btn-new-addon" type="button"
+							   class="btn blr-btn btn-blr-default width-xs-full width-sm-auto float-sm-left"
+							   onclick="showUploadPopup()" value="Upload Add-On"/>
 					</div>
-				</div>
-				<div class="col-xs-12 col-sm-3 col-sm-pull-9 col-md-2 col-md-pull-10">
-					<input id="btn-new-addon" type="button"
-						   class="btn blr-btn btn-blr-default width-xs-full width-sm-auto float-sm-left"
-						   onclick="showUploadPopup()" value="Upload Add-On"/>
-				</div>
-			@else
-				{{-- Do not show login button on the login page. --}}
-				@if(!Request::is('user/login'))
+				@else
 					<div class="col-sm-2 col-sm-offset-10">
 						<input type="button" class="btn blr-btn btn-blr-default float-sm-right"
 							   onclick="showLoginPopup()" value="Log In"/>
 					</div>
 				@endif
-			@endif
-		</div>
-	</nav>
-</div>
+			</div>
+		</nav>
+	</div>
+@endif
