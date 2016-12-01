0607015f5a8b5940092fe098470130f068b3d6ea
mctwist <rajb89@hotmail.com>
2016-12-01 22:24:41 +0100

    Fixed Dropzone script loading for uploading of file

--- resources/views/resources/addon/popup/upload.blade.php
+++ resources/views/resources/addon/popup/upload.blade.php
@@ -1,6 +1,13 @@
 <link rel="stylesheet" type="text/css" href="/css/upload.css">
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/dropzone/4.3.0/min/dropzone.min.js"></script>
-<script type="text/javascript" src="/js/upload-popup.js"></script>
+<script type="text/javascript">
+$(function() {
+	$.getScript('//cdnjs.cloudflare.com/ajax/libs/dropzone/4.3.0/min/dropzone.min.js', function() {
+		$.getScript('/js/upload-popup.js', function() {
+			initDropzone();
+		});
+	});
+});
+</script>
 
 <div id="uploadBox" class="popup-box">
 	{{ Form::open(['route' => 'addon.popup.upload', 'method' => 'put', 'id' => 'uploadAddon', 'files' => true, 'enctype' => 'multipart/form-data']) }}
@@ -11,8 +18,7 @@
 			<div class="row">
 				<div class="file col-xs-12">
 					{{-- Choose File button --}}
-					{{-- FIXME: Fix the Javascript. You need to click the button twice to load a file + you get an error in console, but it works. The dropzone.js is not loaded correctly. --}}
-					{{ Form::button('Choose File', ['onclick' => 'initDropzone()', 'id' => 'dropClick', 'class' => 'btn blr-btn btn-blr-default center-block fileContainer']) }}
+					{{ Form::button('Choose File', ['id' => 'dropClick', 'class' => 'btn blr-btn btn-blr-default center-block fileContainer']) }}
 				</div>
 				<div id="uploadError" class="col-sm-12"></div>
 			</div>
