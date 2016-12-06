c0a7bf88ceaecf9e32906a06a4b735e7408217af
mctwist <rajb89@hotmail.com>
2016-12-06 11:11:52 +0100

    Fixed CategorySeeder to follow 0800b55

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -13,35 +13,35 @@ class CategorySeeder extends Seeder
 	{
 		// Categories with preferences
 		$categories = [
-			['name' => 'Bots', 'tags' => ['Ai', 'Bot'], 'icon' => 'category_bots.png'],
-			['name' => 'Bricks', 'tag' => 'Brick', 'icon' => 'category_bricks.png'],
-			['name' => 'Clients', 'tag' => 'Client', 'icon' => 'category_clients.png'],
-			['name' => 'Client Scripts', 'tag' => 'Client', 'icon' => 'category_client_scripts.png'],
-			['name' => 'Colorsets', 'tag' => 'Colorset', 'icon' => 'category_colorsets.png'],
-			['name' => 'Day Cycles', 'tag' => 'DayCycle', 'icon' => 'category_day_cycles.png'],
-			['name' => 'Decals', 'tag' => 'Decal', 'icon' => 'category_decals.png'],
-			['name' => 'Emotes', 'tag' => 'Emote', 'icon' => 'category_emotes.png'],
-			['name' => 'Environments', 'tag' => 'Environment', 'icon' => 'category_environments.png'],
-			['name' => 'Events', 'tag' => 'Event', 'icon' => 'category_events.png'],
-			['name' => 'Faces', 'tag' => 'Face', 'icon' => 'category_faces.png'],
-			['name' => 'Gamemodes', 'tag' => 'Gamemode', 'icon' => 'category_gamemodes.png'],
-			['name' => 'Grounds', 'tag' => 'Ground', 'icon' => 'category_grounds.png'],
-			['name' => 'Items', 'tag' => 'Item', 'icon' => 'category_items.png'],
-			['name' => 'Lights', 'tag' => 'Light', 'icon' => 'category_lights.png'],
-			['name' => 'Particles', 'tag' => 'Particles', 'icon' => 'category_particles.png'],
-			['name' => 'Players', 'tag' => 'Player', 'icon' => 'category_players.png'],
-			['name' => 'Prints', 'tag' => 'Print', 'icon' => 'category_prints.png'],
-			['name' => 'Projectiles', 'tag' => 'Projectile', 'icon' => 'category_projectiles.png'],
-			['name' => 'Server Scripts', 'tag' => 'Server', 'icon' => 'category_server_scripts.png'],
-			['name' => 'Skies', 'tag' => 'Sky', 'icon' => 'category_skies.png'],
-			['name' => 'Sounds', 'tag' => 'Sound', 'icon' => 'category_sounds.png'],
-			['name' => 'SpeedKart Maps', 'tag' => 'SpeedKart', 'icon' => 'category_saves_speedkart.png'],
-			['name' => 'Support Scripts', 'tag' => 'Support', 'icon' => 'category_support_scripts.png'],
-			['name' => 'Systems', 'tag' => 'System ', 'icon' => 'category_systems.png'],
-			['name' => 'Tools', 'tag' => 'Tool', 'icon' => 'category_tools.png'],
-			['name' => 'Vehicles', 'tag' => 'Vehicle', 'icon' => 'category_vehicles.png'],
-			['name' => 'Waters', 'tag' => 'Water', 'icon' => 'category_waters.png'],
-			['name' => 'Weapons', 'tag' => 'Weapon', 'icon' => 'category_weapons.png'],
+			['name' => 'Bots', 'types' => ['Ai', 'Bot'], 'icon' => 'category_bots.png'],
+			['name' => 'Bricks', 'type' => 'Brick', 'icon' => 'category_bricks.png'],
+			['name' => 'Clients', 'type' => 'Client', 'icon' => 'category_clients.png'],
+			['name' => 'Client Scripts', 'type' => 'Client', 'icon' => 'category_client_scripts.png'],
+			['name' => 'Colorsets', 'type' => 'Colorset', 'icon' => 'category_colorsets.png'],
+			['name' => 'Day Cycles', 'type' => 'DayCycle', 'icon' => 'category_day_cycles.png'],
+			['name' => 'Decals', 'type' => 'Decal', 'icon' => 'category_decals.png'],
+			['name' => 'Emotes', 'type' => 'Emote', 'icon' => 'category_emotes.png'],
+			['name' => 'Environments', 'type' => 'Environment', 'icon' => 'category_environments.png'],
+			['name' => 'Events', 'type' => 'Event', 'icon' => 'category_events.png'],
+			['name' => 'Faces', 'type' => 'Face', 'icon' => 'category_faces.png'],
+			['name' => 'Gamemodes', 'type' => 'Gamemode', 'icon' => 'category_gamemodes.png'],
+			['name' => 'Grounds', 'type' => 'Ground', 'icon' => 'category_grounds.png'],
+			['name' => 'Items', 'type' => 'Item', 'icon' => 'category_items.png'],
+			['name' => 'Lights', 'type' => 'Light', 'icon' => 'category_lights.png'],
+			['name' => 'Particles', 'type' => 'Particles', 'icon' => 'category_particles.png'],
+			['name' => 'Players', 'type' => 'Player', 'icon' => 'category_players.png'],
+			['name' => 'Prints', 'type' => 'Print', 'icon' => 'category_prints.png'],
+			['name' => 'Projectiles', 'type' => 'Projectile', 'icon' => 'category_projectiles.png'],
+			['name' => 'Server Scripts', 'type' => 'Server', 'icon' => 'category_server_scripts.png'],
+			['name' => 'Skies', 'type' => 'Sky', 'icon' => 'category_skies.png'],
+			['name' => 'Sounds', 'type' => 'Sound', 'icon' => 'category_sounds.png'],
+			['name' => 'SpeedKart Maps', 'type' => 'SpeedKart', 'icon' => 'category_saves_speedkart.png'],
+			['name' => 'Support Scripts', 'type' => 'Support', 'icon' => 'category_support_scripts.png'],
+			['name' => 'Systems', 'type' => 'System ', 'icon' => 'category_systems.png'],
+			['name' => 'Tools', 'type' => 'Tool', 'icon' => 'category_tools.png'],
+			['name' => 'Vehicles', 'type' => 'Vehicle', 'icon' => 'category_vehicles.png'],
+			['name' => 'Waters', 'type' => 'Water', 'icon' => 'category_waters.png'],
+			['name' => 'Weapons', 'type' => 'Weapon', 'icon' => 'category_weapons.png'],
 		];
 
 		$insert = [];
@@ -49,21 +49,21 @@ class CategorySeeder extends Seeder
 		// Go through the cateogires to be created
 		foreach ($categories as $category)
 		{
-			// Get tags
-			$tags = [];
-			if (array_key_exists('tag', $category))
+			// Get types
+			$types = [];
+			if (array_key_exists('type', $category))
 			{
-				$tags[] = $category['tag'];
-				unset($category['tag']);
+				$types[] = $category['type'];
+				unset($category['type']);
 			}
-			if (array_key_exists('tags', $category))
-				$tags = array_merge($tags, $category['tags']);
+			if (array_key_exists('types', $category))
+				$types = array_merge($types, $category['types']);
 
 			// Make search faster
-			array_map('strtolower', $tags);
+			array_map('strtolower', $types);
 			// Put together to store away
 			// Note: Using slash as that cannot be used in a path
-			$category['tags'] = implode('/', $tags);
+			$category['types'] = implode('/', $types);
 
 			$insert[] = $category;
 		}
