55ba65c7edd5a8370c55ea1e71eda402ce575fd9
mctwist <rajb89@hotmail.com>
2016-12-10 16:09:25 +0100

    Added so created_at nd updated_at also is random in seeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -18,12 +18,16 @@ $addonFaker->addProvider(new App\Repository\AddonFakerFacade($addonFaker));
 $factory->define(App\Models\User::class, function (Faker\Generator $faker)
 {
 	static $password;
+	$updated_at = $faker->unixTime();
+	$created_at = $faker->unixTime($updated_at);
 
 	return [
 		'username' => $faker->userName,
 		'email' => $faker->unique()->safeEmail,
 		'password' => $password ?: $password = bcrypt('secret'),
 		'remember_token' => str_random(10),
+		'created_at' => $created_at,
+		'updated_at' => $updated_at,
 	];
 });
 
@@ -37,39 +41,55 @@ $factory->define(App\Models\Category::class, function (Faker\Generator $faker)
 
 $factory->define(App\Models\Addon::class, function (Faker\Generator $faker)
 {
+	$updated_at = $faker->unixTime();
+	$created_at = $faker->unixTime($updated_at);
 	$name = $faker->name;
 	return [
 		'name' => $name,
 		'slug' => str_slug($name, '_'),
 		'description' => $faker->text(512),
+		'created_at' => $created_at,
+		'updated_at' => $updated_at,
 	];
 });
 
 $factory->define(App\Models\Channel::class, function (Faker\Generator $faker)
 {
+	$updated_at = $faker->unixTime();
+	$created_at = $faker->unixTime($updated_at);
 	return [
 		'name' => $faker->randomElement(['debug', 'test']),
 		'slug' => str_random(10),
 		'description' => $faker->text(512),
+		'created_at' => $created_at,
+		'updated_at' => $updated_at,
 	];
 });
 
 $factory->define(App\Models\Version::class, function (Faker\Generator $faker)
 {
+	$updated_at = $faker->unixTime();
+	$created_at = $faker->unixTime($updated_at);
 	return [
 		'name' => '0',
 		'change_log' => $faker->text(128),
+		'created_at' => $created_at,
+		'updated_at' => $updated_at,
 	];
 });
 
 $factory->define(App\Models\File::class, function (Faker\Generator $faker)
 	use(&$addonFaker)
 {
+	$updated_at = $faker->unixTime();
+	$created_at = $faker->unixTime($updated_at);
 	return [
 		'display_name' => $addonFaker->addon_name,
 		'path' => $addonFaker->md5.'.zip',
 		'size' => $addonFaker->numberBetween(1000, 50000000),
 		'extension' => 'zip',
 		'mime' => 'application/zip',
+		'created_at' => $created_at,
+		'updated_at' => $updated_at,
 	];
 });
