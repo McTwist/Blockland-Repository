e0ed97fe43b7ca86cab3a1b25b67b162cae581e7
mctwist <rajb89@hotmail.com>
2016-12-13 08:12:07 +0100

    Removed the old Utilities section as we're not using traits

--- app/Models/Addon.php
+++ app/Models/Addon.php
@@ -374,49 +374,4 @@ class Addon extends Model
 	{
 		return '/api/mod/'.$this->slug.'.zip';
 	}
-
-	/////////////////
-	//* Utilities *//
-	/////////////////
-	/**
-	 * Calculates metric bytes prefixes.
-	 *
-	 * @param  int  $bytes
-	 * @param  int  $precision
-	 *
-	 * @return string
-	 */
-	protected static function formatBytesMet($bytes, $precision = 2)
-	{ 
-		$units = array('B', 'KB', 'MB', 'GB', 'TB');
-
-		$bytes = max($bytes, 0);
-		$pow = floor(($bytes ? log($bytes) : 0) / log(1000));
-		$pow = min($pow, count($units) - 1);
-
-		$bytes /= pow(1000, $pow);
-
-		return round($bytes, $precision) . ' ' . $units[$pow];
-	}
-
-	/**
-	 * Calculates binary bytes prefixes.
-	 *
-	 * @param  int  $bytes
-	 * @param  int  $precision
-	 *
-	 * @return string
-	 */
-	protected static function formatBytesBin($bytes, $precision = 2)
-	{ 
-		$units = array('B', 'KiB', 'MiB', 'GiB', 'TiB');
-
-		$bytes = max($bytes, 0);
-		$pow = floor(($bytes ? log($bytes) : 0) / log(1024));
-		$pow = min($pow, count($units) - 1);
-
-		$bytes /= (1 << (10 * $pow));
-
-		return round($bytes, $precision) . ' ' . $units[$pow];
-	}
 }
