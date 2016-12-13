300000ad76308c224de1782828f2a47f1374ad5e
mctwist <rajb89@hotmail.com>
2016-12-13 14:55:19 +0100

    Added datalist macro

---
+++ resources/macros/datalist.macro.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Creates datalist element with options per definition of HTML5
+ */
+Form::macro('datalist', function ($name, $list = [], $key_value = false, $options = [])
+{
+	$options['id'] = $this->getIdAttribute($name, $options);
+
+	if (!isset($options['id']))
+	{
+		$options['id'] = $name;
+	}
+
+	$html = [];
+
+	foreach ($list as $value => $display)
+	{
+		$html[] = $this->option($display, ($key_value ? $value : null), false);
+	}
+
+    $options = $this->html->attributes($options);
+
+    $list = implode('', $html);
+
+    return $this->toHtmlString("<datalist{$options}>{$list}</datalist>");
+});
+?>
