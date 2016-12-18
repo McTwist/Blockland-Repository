5600698b389d4ffa1f751dae05ff2bdaac40ca53
Demian Wright <demian.git@gmail.com>
2016-12-19 00:23:14 +0200

    Changes user repositories into a list (was a grid) in user profiles

--- resources/views/auth/show.blade.php
+++ resources/views/auth/show.blade.php
@@ -5,27 +5,11 @@
 @section('content')
 	<div class="container-fluid">
 		<div class="row">
-			<h1 class="title mar-xs-btm-half">
+			<h1 class="mar-xs-btm-half">
 				{{ $user->username }}
 			</h1>
 		</div>
 		<div class="row">
-			@if(Auth::id() === $user->id)
-				<div class="col-xs-12 nopad-xs-both mar-xs-btm col-sm-4 col-md-3 pull-right">
-					<a class="btn blr-btn btn-blr-default width-xs-full width-sm-auto pull-right"
-					   href="{{ route('user.edit') }}">Change Account Settings</a>
-				</div>
-			@endif
-			{{-- Repositories --}}
-			<div class="col-xs-12 col-sm-6">
-				@forelse($user->repositories as $repo)
-					<div class="col-xs-12 text-xs-left col-sm-5 text-sm-right col-md-4 blr-label">
-						<a href="{{ $repo->route }}"><div>{{ $repo->name }}</div></a>
-					</div>
-				@empty
-					This user has no uploaded files.
-				@endforelse
-			</div>
 			{{-- Blockland info --}}
 			@if($user->bl_id != -1)
 				<div class="col-xs-12 col-sm-6">
@@ -37,7 +21,6 @@
 							<p class="text-xs-center text-sm-left blr-value">{{ $user->bl_name }}</p>
 						</div>
 					</div>
-
 					<div class="row">
 						<div class="col-xs-12 text-xs-left col-sm-5 text-sm-right col-md-4 blr-label">
 							Blockland ID:
@@ -48,6 +31,33 @@
 					</div>
 				</div>
 			@endif
+			@if(Auth::id() === $user->id)
+				<div class="col-xs-12 nopad-xs-both mar-xs-btm col-sm-4 col-md-3 pull-right">
+					<a class="btn blr-btn btn-blr-default width-xs-full width-sm-auto pull-right"
+					   href="{{ route('user.edit') }}">Change Account Settings</a>
+				</div>
+			@endif
+		</div>
+		<div class="row">
+			<h2 class="mar-xs-btm-half">Repositories</h2>
+		</div>
+		<div class="row">
+			<div class="col-xs-12 hug-xs-left">
+				@if(count($user->repositories) > 0)
+					<ul class="user-repo-list">
+						@foreach($user->repositories as $repo)
+							<li><a href="{{ $repo->route }}"><span class="blr-label">{{ $repo->name }}:</span>
+									@if(!empty($repo->summary))
+										<span class="blr-value"> {{ $repo->summary }}</span>
+									@else
+										<span class="blr-value --italic"> No summary.</span>
+									@endif</a></li>
+						@endforeach
+					</ul>
+				@else
+					<p>{{ $user->username }} has no uploaded files.</p>
+				@endif
+			</div>
 		</div>
 	</div>
 @endsection
